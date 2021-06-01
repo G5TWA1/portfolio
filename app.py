918ec08940f92a1476c47eebb5e089e1566f41ca
G5TWA1 <aarr1@sapo.pt>
2021-06-01 14:24:03 +0100

    teste2

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contacts.html")
+
+@app.route("/projects/")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/experience/")
+def experience():
+    return render_template("experience.html")
+
+if __name__ == '__main__':
+    app.run()
